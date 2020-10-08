6e91408740ed77c522fcc086cfb697c7fe77edc1
finn <finn@Finn>
2020-10-08 23:45:39 +0200

    Added Texture Class

--- src/me/finnthehumanlol/lwjgl/engine/types/Texture.java
+++ src/me/finnthehumanlol/lwjgl/engine/types/Texture.java
@@ -4,33 +4,73 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
 
 import javax.security.auth.callback.TextInputCallback;
 
-import org.newdawn.slick.*;
-import org.newdawn.slick.opengl.TextureLoader;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.stb.STBImage;
+import org.lwjgl.system.MemoryStack;
 
 public class Texture {
 
 	private int textureID;
 	private ByteBuffer image;
 	private String texturePath;
+	private int texWidth, texHeight;
 	
 	public Texture(String texturePath) {
-		this.texturePath = texturePath;
-		org.newdawn.slick.opengl.Texture texture = null;
+		int textureID;
 		
-		try {
-			texture = TextureLoader.getTexture("PNG", new FileInputStream(texturePath));
-		}catch(FileNotFoundException e) {
-			e.printStackTrace();
-		}catch(IOException e) {
-			e.printStackTrace();
+		int width, height;
+		ByteBuffer image;
+		
+		try(MemoryStack stack = MemoryStack.stackPush()){
+			IntBuffer w = stack.mallocInt(1);
+			IntBuffer h = stack.mallocInt(1);
+			IntBuffer comp = stack.mallocInt(1);
+		
+			image = STBImage.stbi_load(texturePath, w, h, comp, 4);
+			
+			if(image == null) {
+				System.err.println("Couldnt load texture!");
+			}
+			
+			width = w.get();
+			height = h.get();
+			this.texWidth = width;
+			this.texHeight = height;
+			
 		}
+
+		textureID = GL11.glGenTextures();
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureID);
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, width, height, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, image);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
 		
-		this.textureID = texture.getTextureID();
+		this.textureID = textureID;
+	}
+
+	public int getTextureID() {
 		return textureID;
-		
+	}
+
+	public ByteBuffer getImage() {
+		return image;
+	}
+
+	public String getTexturePath() {
+		return texturePath;
+	}
+
+	public int getTexWidth() {
+		return texWidth;
+	}
+
+	public int getTexHeight() {
+		return texHeight;
 	}
 	
 }
--- src/me/finnthehumanlol/lwjgl/project/Main.java
+++ src/me/finnthehumanlol/lwjgl/project/Main.java
@@ -2,17 +2,14 @@ package me.finnthehumanlol.lwjgl.project;
 
 import me.finnthehumanlol.lwjgl.engine.Engine;
 import me.finnthehumanlol.lwjgl.engine.core.MainGameLoop;
+import me.finnthehumanlol.lwjgl.engine.render.Renderer;
+import me.finnthehumanlol.lwjgl.engine.types.Mesh;
 
 public class Main {
-
-	public Mesh mesh 
 	
 	public static MainGameLoop mainGameLoop = new MainGameLoop() {
-
-		@Override
-		public void start() {
-			
-		}
+		
+		Mesh mesh;
 		
 		@Override
 		public void update() {
@@ -20,18 +17,30 @@ public class Main {
 		}
 		
 		@Override
-		public void render() {
-			
-		}
-
-		@Override
 		public void stop() {
 			
+		}
+		
+		@Override
+		public void start() {
+			mesh = new Mesh(new float[] {
+					-0.5f, -0.5f, 1.0f,
+					0.0f, 0.5f, 1.0f, 
+					0.5f, -0.5f, 1.0f
+			}, new int[] {
+					0, 1, 2
+			});
+			mesh.attachShader();
+		}
+		
+		@Override
+		public void render() {
+			Renderer.render(mesh);
 		}
 	};
 	
 	public static void main(String[] args) {
-		Engine.createWindow(1280, 720, "window");
+		Engine.createWindow(1280, 720, "Window");
 		Engine.start(mainGameLoop);
 	}
 
